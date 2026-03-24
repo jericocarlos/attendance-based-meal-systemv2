6af1acf93c12f897620209474557064c9e16f50f
jericocarlos <jericonielc@gmail.com>
2026-03-25 07:54:42 +0800

    Revert "Update db.js."
    This reverts commit e82146c49464c207d8598c3b60b3da84ae4182fd.

--- app/api/attendance/add/route.js
+++ app/api/attendance/add/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { executeQuery, attendancePool } from '@/lib/db';
+import { executeQuery, attendancePool, freemealPool } from '@/lib/db';
 // import { time } from 'zod';
 
 export async function POST(request) {
@@ -112,6 +112,7 @@ export async function POST(request) {
       query: latestLogQuery,
       values: [employee.ashima_id, timeForQueries],
     });
+    const freemealConn = await freemealPool.getConnection();
     const attendanceConn = await attendancePool.getConnection();
 
     let nextLogType = "CLAIMED";
